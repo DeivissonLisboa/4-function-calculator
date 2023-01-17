dc30e001d21686c964009f4f10033142f3f57d7b
Deivisson Lisboa <dvilaflor@gmail.com>
2023-01-17 13:05:01 -0300

    Create Calculator class

---
+++ src/script/calculator.js
@@ -0,0 +1,17 @@
+class Calculator {
+  constructor(display) {
+    this.display = display
+    this.previuos = ""
+    this.clear()
+  }
+
+  clear() {}
+
+  appendNumber(number) {}
+
+  chooseOperation(operation) {}
+
+  compute() {}
+
+  updateDisplay() {}
+}
