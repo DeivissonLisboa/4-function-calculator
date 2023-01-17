ba8166e16bd73c1b10501ab81182d6dd3778c489
Deivisson Lisboa <dvilaflor@gmail.com>
2023-01-17 12:50:31 -0300

    Call all buttons to script

---
+++ src/script/script.js
@@ -0,0 +1,5 @@
+const digitBtns = document.querySelectorAll(".numbers")
+const operationBtns = document.querySelectorAll(".operations")
+const equalBtn = document.querySelector(".equal")
+const clearBtn = document.querySelector(".clr")
+const resultText = document.querySelector("#result")
