31a8c4fa9247cf75e22ce5c77f735904026def5e
Deivisson Lisboa <dvilaflor@gmail.com>
2023-01-17 13:16:40 -0300

    Instantiate Calculator

--- src/script/script.js
+++ src/script/script.js
@@ -3,3 +3,5 @@ const operationBtns = document.querySelectorAll(".operations")
 const equalBtn = document.querySelector(".equal")
 const clearBtn = document.querySelector(".clr")
 const resultText = document.querySelector("#result")
+
+const calculator = new Calculator(resultText)
